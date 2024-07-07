536ed1b7beb44329b85bc6b1a00568dce7660889
fanzh <fanzhlinux@163.com>
2024-07-07 17:04:13 +0800

    添加MustToYaml

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Fanzh-code-home/mstools
 
 go 1.21.6
+
+require sigs.k8s.io/yaml v1.4.0
---
+++ my_tools/writeToYaml.go
@@ -0,0 +1,11 @@
+package myTools
+
+import "sigs.k8s.io/yaml"
+
+func MustToYaml(v any) string {
+	b, err := yaml.Marshal(v)
+	if err != nil {
+		panic(err)
+	}
+	return string(b)
+}
